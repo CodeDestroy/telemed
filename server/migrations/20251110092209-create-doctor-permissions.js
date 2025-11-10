b715591524e4660810f47d82f0b7253612046db3
CodeDestroy <andrei_novichihin@mail.ru>
2025-11-10 12:22:59 +0300

    create permissions

---
+++ server/migrations/20251110092209-create-doctor-permissions.js
@@ -0,0 +1,34 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('DoctorPermissions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      doctorId: {
+        type: Sequelize.INTEGER
+      },
+      permissionId: {
+        type: Sequelize.INTEGER
+      },
+      grantedBy: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('DoctorPermissions');
+  }
+};
